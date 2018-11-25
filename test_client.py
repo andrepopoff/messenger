a162f8b0f49b518d1210566f2e95292ad8d6547a
andrepopoff <andrepopoff@gmail.com>
2018-11-25 19:32:39 +0400

    Wrote test_send_to_server() function (test_client.py)

--- test_client.py
+++ test_client.py
@@ -1,3 +1,6 @@
+import socket
+import json
+
 from pytest import raises
 from client import create_message, send_to_server, get_message_from_server, check_answer
 
@@ -5,3 +8,31 @@ from client import create_message, send_to_server, get_message_from_server, chec
 def test_create_message():
     message = create_message()
     assert message['action'] == 'presence'
+
+
+class ClientSocket:
+    """
+    Class for socket operations
+    """
+    def __init__(self, sock_type=socket.AF_INET, sock_family=socket.SOCK_STREAM):
+        pass
+
+    def recv(self, n):
+        message = {'response': 200}
+        json_message = json.dumps(message)
+        byte_message = json_message.encode('utf-8')
+        return byte_message
+
+    def send(self, byte_message):
+        pass
+
+
+def test_send_to_server(monkeypatch):
+    monkeypatch.setattr('socket.socket', ClientSocket)
+    sock = socket.socket()
+
+    assert send_to_server(sock, {'any_key': 'any_value'}) is None
+
+    with raises(TypeError):
+        send_to_server('not_a_dict')
+
