4b01ffc3f9bf0942cc132528988bb6a02d523e71
andrepopoff <andrepopoff@gmail.com>
2018-11-25 16:51:12 +0400

    Wrote the logic of the Client

--- client.py
+++ client.py
@@ -1,7 +1,23 @@
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('localhost', 8888))
-time = sock.recv(1024)
-sock.close()
-print(time.decode('utf-8'))
+
+def create_message():
+    pass
+
+
+def send_to_server(message):
+    pass
+
+
+def get_message_from_server():
+    pass
+
+
+if __name__ == '__main__':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('localhost', 8888))
+    message = create_message()
+    send_to_server(message)
+    answer = get_message_from_server()
+    sock.close()
+    print(answer.decode('utf-8'))
