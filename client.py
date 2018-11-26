c62cc5553e0b1e24a9f28026e891cfe896ac1c9c
andrepopoff <andrepopoff@gmail.com>
2018-11-26 21:49:25 +0400

    Added comment about mode (client.py)

--- client.py
+++ client.py
@@ -88,10 +88,10 @@ if __name__ == '__main__':
     checked_answer = check_answer(answer)
 
     if answer['response'] == 200:
+        # depending on the mode we will listen or send messages
         if mode == 'r':
             read_messages()
         elif mode == 'w':
             write_messages()
         else:
             raise Exception('Wrong mode!')
-        
