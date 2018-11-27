fbac76a6eed20ceb677fff425c9bebe792485a09
andrepopoff <andrepopoff@gmail.com>
2018-11-27 13:08:59 +0400

    Created config.py with jim-protocol constants and settings

---
+++ config.py
@@ -0,0 +1,24 @@
+"""
+Constants for jim-protocol, settings
+"""
+
+# Keys
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+RESPONSE = 'response'
+ERROR = 'error'
+
+# Values
+PRESENSE = 'presence'
+MSG = 'message'
+TO = 'to'
+FROM = 'from'
+
+# Response codes
+BASIC_NOTICE = 100
+OK = 200
+ACCEPTED = 202
+WRONG_REQUEST = 400
+SERVER_ERROR = 500
