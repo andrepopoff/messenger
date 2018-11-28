5b7f3f9101605374dbff1869ff813c8d6c94b34b
andrepopoff <andrepopoff@gmail.com>
2018-11-28 17:12:49 +0400

    Created class User in client.py. Moved all functions to class methods

--- client.py
+++ client.py
@@ -14,81 +14,85 @@ import sys
 import time
 import json
 
-from config import ACTION, PRESENCE, TIME, MSG, TO, FROM, USER, ACCOUNT_NAME, MESSAGE
+from config import *
 
 
-def write_messages(client_sock):
-    """
-    The client writes a message in an infinite loop.
-    """
-    while True:
-        text = input('Enter text: ')
-        message = create_message(MSG, text=text)
-        send_to_server(client_sock, message)
+class User:
+    def __init__(self, login, address, port):
+        self.__login = login
+        self.__address = address
+        self.__port = port
+        self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    def connect(self):
+        self.__sock.connect((self.__address, self.__port))
 
-def read_messages(client_sock):
-    """
-    Client reads incoming messages in an infinite loop
-    """
-    while True:
-        message = get_message_from_server(client_sock)
-        print(message)
+    def create_message(self, action, to=None, text=None):
+        if action == PRESENCE:
+            return {
+                ACTION: PRESENCE,
+                TIME: time.time(),
+                USER: {
+                    ACCOUNT_NAME: self.__login
+                }
 
-
-def create_message(action, to=None, account_name=None, text=None):
-    if action == PRESENCE:
-        return {
-            ACTION: PRESENCE,
-            TIME: time.time(),
-            USER: {
-                ACCOUNT_NAME: account_name
+            }
+        elif action == MSG:
+            return {
+                ACTION: MSG,
+                TIME: time.time(),
+                TO: to,
+                FROM: self.__login,
+                MESSAGE: text
             }
 
-        }
-    elif action == MSG:
-        return {
-            ACTION: MSG,
-            TIME: time.time(),
-            TO: to,
-            FROM: account_name,
-            MESSAGE: text
-        }
-
-
-def send_to_server(client_socket, message):
-    if isinstance(message, dict):
-        json_message = json.dumps(message)
-        byte_message = json_message.encode('utf-8')
-        client_socket.send(byte_message)
-    else:
-        raise TypeError
-
-
-def get_message_from_server(client_socket):
-    byte_answer = client_socket.recv(1024)
-    if isinstance(byte_answer, bytes):
-        json_answer = byte_answer.decode('utf-8')
-        answer = json.loads(json_answer)
-        if isinstance(answer, dict):
-            return answer
+    def send_to_server(self, message):
+        if isinstance(message, dict):
+            json_message = json.dumps(message)
+            byte_message = json_message.encode('utf-8')
+            self.__sock.send(byte_message)
         else:
             raise TypeError
-    else:
-        raise TypeError
 
+    def get_message_from_server(self):
+        byte_answer = self.__sock.recv(1024)
+        if isinstance(byte_answer, bytes):
+            json_answer = byte_answer.decode('utf-8')
+            answer = json.loads(json_answer)
+            if isinstance(answer, dict):
+                return answer
+            else:
+                raise TypeError
+        else:
+            raise TypeError
 
-def check_answer(answer):
-    if not isinstance(answer, dict):
-        raise TypeError
-    if 'response' not in answer:
-        raise KeyError
-    return answer
+    @staticmethod
+    def check_answer(answer):
+        if not isinstance(answer, dict):
+            raise TypeError
+        if 'response' not in answer:
+            raise KeyError
+        return answer
+
+    def read_messages(self):
+        """
+        Client reads incoming messages in an infinite loop
+        """
+        while True:
+            message = self.get_message_from_server()
+            print(message)
+
+    def write_messages(self):
+        """
+        The client writes a message in an infinite loop.
+        """
+        while True:
+            text = input('Enter text: ')
+            message = self.create_message(MSG, text=text)
+            self.send_to_server(message)
 
 
 if __name__ == '__main__':
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     try:
         address = sys.argv[1]
     except IndexError:
@@ -107,17 +111,18 @@ if __name__ == '__main__':
     except IndexError:
         mode = 'r'
 
-    sock.connect((address, port))
-    message = create_message(PRESENCE)
-    send_to_server(sock, message)
-    answer = get_message_from_server(sock)
-    checked_answer = check_answer(answer)
+    user = User(input('Enter a login: '), address, port)
+    user.connect()
+    message = user.create_message(PRESENCE)
+    user.send_to_server(message)
+    answer = user.get_message_from_server()
+    checked_answer = user.check_answer(answer)
 
-    if answer['response'] == 200:
+    if answer[RESPONSE] == OK:
         # depending on the mode we will listen or send messages
         if mode == 'r':
-            read_messages(sock)
+            user.read_messages()
         elif mode == 'w':
-            write_messages(sock)
+            user.write_messages()
         else:
             raise Exception('Wrong mode!')
